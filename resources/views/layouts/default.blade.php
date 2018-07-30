2dbb6fc90b674b4dd2e06d38014ed1c255b130c5
zhusong666 <851119622@qq.com>
2018-07-30 17:55:46 +0800

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title','laravel_框架学习')-我的Laravel</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title','帮助页')
+
+@section('content')
+<h1>帮助页</h1>
+@stop
