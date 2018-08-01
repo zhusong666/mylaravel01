5faf7272e678d114bff3c0d606ae791281ea092b
zhusong666 <851119622@qq.com>
2018-08-01 16:51:28 +0800

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 //     return view('welcome');
 // });
 
-Route::get('/', 'StaticPagesController@home')->name('/');
+Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
@@ -23,3 +23,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 //user资源路由
 Route::resource('users', 'UsersController');
+//登录路由
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
